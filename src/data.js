b5516454eb23996ac14effabc14a488d5e085989
radinreth <radin.reth@gmail.com>
2019-09-25 15:56:58 +0700

    update mocked data

--- src/data.js
+++ src/data.js
@@ -1,80 +1,110 @@
 const data = [
     {
-      id: 1,
-      name: 'Decease Outbreak',
-      items: [
-        {
-          id: '1',
-          desc: 'lorem ipsum 1'
-        },
-        {
-          id: '2',
-          desc: 'lorem ipsum 1'
-        }
-      ],
-      description:
-        'a brief description of H5N1 \n a brief description of H5N1 \n a brief description of H5N1 \n a brief description of H5N1 \n ',
+      _source: {
+        id: 1,
+        document_type: 'document',
+        name: 'Decease Outbreak',
+        items: [
+          {
+            id: '1',
+            desc: 'lorem ipsum 1'
+          },
+          {
+            id: '2',
+            desc: 'lorem ipsum 1'
+          }
+        ],
+        description:
+          'a brief description of H5N1 \n a brief description of H5N1 \n a brief description of H5N1 \n a brief description of H5N1 \n ',
+      },
+      highlight: {
+        name: ["<em class='highlight'>decease<\/em>"]
+      }
     },
     {
-      id: 2,
-      name: 'Human',
-      items: [
-        {
-          id: '1',
-          desc: 'lorem ipsum 1'
-        },
-        {
-          id: '2',
-          desc: 'lorem ipsum 1'
-        }
-      ],
-      description:
-        'a brief description of Influenza \n a brief description of Influenza \n a brief description of Influenza \n a brief description of Influenza \n ',
+      _source: {
+        id: 2,
+        document_type: 'folder',
+        name: 'Human',
+        items: [
+          {
+            id: '1',
+            desc: 'lorem ipsum 1'
+          },
+          {
+            id: '2',
+            desc: 'lorem ipsum 1'
+          }
+        ],
+        description:
+          'a brief description of Influenza \n a brief description of Influenza \n a brief description of Influenza \n a brief description of Influenza \n ',
+      },
+      highlight: {
+        name: ["<em class='highlight'>human<\/em>"]
+      }
     },
     {
-      id: 3,
-      name: 'Animal',
-      items: [
-        {
-          id: '1',
-          desc: 'lorem ipsum 1'
-        },
-        {
-          id: '2',
-          desc: 'lorem ipsum 1'
-        }
-      ],
-      description: 'BUNDLE  [Anz, dev] ./index.j',
+      _source: {
+        id: 3,
+        document_type: 'document',
+        name: 'Animal',
+        items: [
+          {
+            id: '1',
+            desc: 'lorem ipsum 1'
+          },
+          {
+            id: '2',
+            desc: 'lorem ipsum 1'
+          }
+        ],
+        description: 'BUNDLE  [Anz, dev] ./index.j',
+      },
+      highlight: {
+        name: ["<em class='highlight'>animal<\/em>"]
+      }
     },
     {
-      id: 4,
-      name: 'Environment',
-      items: [
-        {
-          id: '1',
-          desc: 'lorem ipsum 1'
-        },
-        {
-          id: '2',
-          desc: 'lorem ipsum 1'
-        }
-      ],
-      description: 'BUNDLE  [Anz, dev] ./index.j',
+      _source: {
+        id: 4,
+        document_type: 'document',
+        name: 'Environment',
+        items: [
+          {
+            id: '1',
+            desc: 'lorem ipsum 1'
+          },
+          {
+            id: '2',
+            desc: 'lorem ipsum 1'
+          }
+        ],
+        description: 'BUNDLE  [Anz, dev] ./index.j',
+      },
+      highlight: {
+        name: ["<em class='highlight'>environment<\/em>"]
+      }
     },
     {
-      id: 5,
-      name: 'Cross Border',
-      items: [
-        {
-          id: '1',
-          desc: 'lorem ipsum 1'
-        },
-        {
-          id: '2',
-          desc: 'lorem ipsum 1'
-        }
-      ],
-      description: 'BUNDLE  [Anz, dev] ./index.j',
+      _source: {
+        id: 5,
+        document_type: 'document',
+        name: 'Cross Border',
+        items: [
+          {
+            id: '1',
+            desc: 'lorem ipsum 1'
+          },
+          {
+            id: '2',
+            desc: 'lorem ipsum 1'
+          }
+        ],
+        description: 'BUNDLE  [Anz, dev] ./index.j',
+      },
+      highlight: {
+        name: ["<em class='highlight'>cross<\/em>"]
+      }
     },
   ];
 
