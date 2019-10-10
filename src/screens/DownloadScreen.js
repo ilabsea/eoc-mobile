d68280bef1546c245afa8a774f0bd50580390c2e
radinreth <radin.reth@gmail.com>
2019-10-10 13:34:05 +0700

    destroy from melon

--- src/screens/DownloadScreen.js
+++ src/screens/DownloadScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Icon, List, ListItem, Text, Footer, Header, H3, Title,
+import { Icon, List, ListItem, Text, Footer, Header, H3, Title, Button,
           Content, Container, Body, Left, Right, Subtitle } from 'native-base'
 import database from '../model/db'
 
@@ -11,6 +11,7 @@ class DownloadScreen extends React.Component {
     }
 
     this.downloadCollection = database.collections.get('downloads')
+    this.getAllDownloads = this.getAllDownloads.bind(this)
   }
 
   componentDidMount() {
@@ -19,8 +20,34 @@ class DownloadScreen extends React.Component {
 
   getAllDownloads = async () => {
     const all = await this.downloadCollection.query().fetch()
-    const downloads = all.map(d => [{ name: d.name, localUrl: d.localUrl }])
+    const downloads = all.map(d => [{ id: d.id, name: d.name, localUrl: d.localUrl }])
     this.setState({ downloads })
+  }
+
+  async gen() {
+    const db = await database.action(async () => {
+      const newDownload = await this.downloadCollection.create(dl => {
+        dl.remoteUrl = 'test remote url'
+        dl.localUrl = 'test local url'
+        dl.name = 'test name'
+      })
+      console.log('created')
+    })
+  }
+
+  async remove(id) {
+    await this.downloadCollection.find(id)
+      .then( async (r) => {
+
+        await database.action(async () => {
+          await r.markAsDeleted()
+          await r.destroyPermanently()
+        })
+
+        console.log('removed')
+        this.getAllDownloads()
+      })
+      .catch( e => console.log('error caught: ', e))
   }
 
   render() {
@@ -37,11 +64,22 @@ class DownloadScreen extends React.Component {
         </Header>
 
         <Content padder>
+          <Button full onPress={ this.gen.bind(this) }>
+            <Text>Gen.</Text>
+          </Button>
+
           <List>
             {
               downloads.map ( d => {
-                return <ListItem>
-                  <Text>{ d[0].name }</Text>
+                return <ListItem key={d[0].name}>
+                  <Left>
+                    <Text>{ d[0].name }</Text>
+                  </Left>
+                  <Right>
+                    <Button small danger onPress={this.remove.bind(this, d[0].id )}>
+                      <Icon name="md-remove-circle-outline" />
+                    </Button>
+                  </Right>
                 </ListItem>
               })
             }
