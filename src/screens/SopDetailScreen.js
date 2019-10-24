62310ab448a7ee0cf78a462e1c5668397e068842
radinreth <radin.reth@gmail.com>
2019-10-24 14:44:56 +0700

    Update ui nested component

--- src/screens/SopDetailScreen.js
+++ src/screens/SopDetailScreen.js
@@ -1,16 +1,56 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { Container, Button, Content, Header, Left, 
+import { Container, Button, Content, Header, Left, H3,
           Right, Body, Title, Icon, List, ListItem } from 'native-base'
 import moment from 'moment';
 import { service } from '../services';
 import { withNavigation } from 'react-navigation'
+import styleUtils from '../config/styles'
+
+class Item extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (<ListItem thumbnail>
+      <Left>
+        <Button transparent style={styleUtils.btnIcon}>
+          <Icon style={{ fontSize:42 }} />
+        </Button>
+      </Left>
+      <Body>
+        <View>
+          <Text>
+            <H3>{name}</H3>
+          </Text>
+
+          <View style={{ flexDirection: 'row', alignItems:'center', marginTop: 10 }}>
+            <Icon name="md-time" style={{ fontSize: 20, marginRight: 5, color: "#666666" }} />
+            <Text style={{color: "#666666"}}>
+              { moment(created_at).fromNow() }
+            </Text>
+          </View>
+        </View>
+      </Body>
+      <Right>
+        <Button rounded
+                onPress={() => this.navigate(id)}>
+          <Icon name="arrow-forward" />
+        </Button>
+      </Right>
+    </ListItem>
+    )
+  }
+}
 
 class SopDetailScreen extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      categories: []
+      sops: [],
+      children: []
     }
   }
 
@@ -21,9 +61,9 @@ class SopDetailScreen extends React.Component {
       console.log(sopGuide)
       const id = navigation.getParam('id')
 
-      let categories = await service.apiManager.fetch_category_children(sopGuide.id)
+      let { sops, children } = await service.apiManager.fetch_category_children(sopGuide.id)
       this.setState({
-        categories
+        sops, children
       })
     } catch( e) {
       console.log(e)
@@ -31,6 +71,7 @@ class SopDetailScreen extends React.Component {
   }
 
   navigate(c) {
+    console.log("c", c)
     this.props.navigation.push('SopDetail', { sopGuide: c, id: c.id })
   }
 
@@ -47,16 +88,81 @@ class SopDetailScreen extends React.Component {
             </Button>
           </Left>
           <Body>
-            <Title>{ 'hi' }</Title>
+            <Title>{ sopGuide.name }</Title>
           </Body>
           <Right />
         </Header>
         <Content padder>
+          <H3>Sops:</H3>
           <List>
             {
-              this.state.categories.map( c => {
-                return <ListItem key={c.id} onPress={ () => this.navigate(c) }>
-                  <Text>{c.name}</Text>
+              this.state.sops.map( c => {
+                return <ListItem thumbnail key={c.id}>
+                  <Left>
+                    <Button transparent style={styleUtils.btnIcon}>
+                      <Icon type="MaterialIcons" style={{ fontSize:42 }} name="insert-drive-file" color="#6E0505" />
+                    </Button>
+                  </Left>
+
+                  <Body>
+                    <View>
+                      <Text>
+                        <H3>{c.name}</H3>
+                      </Text>
+
+                      <View style={{ flexDirection: 'row', alignItems:'center', marginTop: 10 }}>
+                        <Icon name="md-time" style={{ fontSize: 20, marginRight: 5, color: "#666666" }} />
+                        <Text style={{color: "#666666"}}>
+                          { moment(c.created_at).fromNow() }
+                        </Text>
+                      </View>
+                    </View>
+                  </Body>
+
+                  <Right>
+                    <Button rounded
+                            onPress={() => this.navigate(c)}>
+                      <Icon type="MaterialIcons" name="file-download" />
+                    </Button>
+                  </Right>
+
+                </ListItem>
+              })
+            }
+          </List>
+
+          <H3>Children:</H3>
+          <List>
+            {
+              this.state.children.map( c => {
+                return <ListItem thumbnail key={c.id}>
+                  <Left>
+                    <Button transparent style={styleUtils.btnIcon}>
+                      <Icon style={{ fontSize:42 }} name="folder" color="gray" />
+                    </Button>
+                  </Left>
+
+                  <Body>
+                    <View>
+                      <Text>
+                        <H3>{c.name}</H3>
+                      </Text>
+
+                      <View style={{ flexDirection: 'row', alignItems:'center', marginTop: 10 }}>
+                        <Icon name="md-time" style={{ fontSize: 20, marginRight: 5, color: "#666666" }} />
+                        <Text style={{color: "#666666"}}>
+                          { moment(c.created_at).fromNow() }
+                        </Text>
+                      </View>
+                    </View>
+                  </Body>
+
+                  <Right>
+                    <Button rounded
+                            onPress={() => this.navigate(c)}>
+                      <Icon type="MaterialIcons" name="arrow-forward" />
+                    </Button>
+                  </Right>
                 </ListItem>
               })
             }
@@ -68,8 +174,6 @@ class SopDetailScreen extends React.Component {
 }
 
 SopDetailScreen.navigationOptions = navigationData => {
-  const sopGuide = navigationData.navigation.getParam('sopGuide');
-
   return {
     headerStyle: {
       backgroundColor: '#6b52ae',
@@ -77,13 +181,5 @@ SopDetailScreen.navigationOptions = navigationData => {
     headerTintColor: '#fff',
   }
 }
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
 
 export default withNavigation(SopDetailScreen)
