9fa69c983448319aab7c8d1f0a68f978314992b6
radinreth <radin.reth@gmail.com>
2019-10-28 10:30:53 +0700

    Update sopdetail screen

--- src/screens/SopDetailScreen.js
+++ src/screens/SopDetailScreen.js
@@ -1,13 +1,36 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Alert } from 'react-native'
+import { Container, Content, Text, Header, 
+      Left, Right, Body, Title, Button, Icon, H1 } from 'native-base'
 
 class SopDetailScreen extends React.Component {
   render() {
-    let { item } = this.props.navigation.getParam('item')
+    let item = this.props.navigation.getParam('item')
 
-    return <View>
-      <Text>{item.name}</Text>
-    </View>
+    return <Container>
+      <Header>
+        <Left>
+          <Button transparent 
+                  onPress={ () => this.props.navigation.goBack() }>
+            <Icon name="md-arrow-back" />
+          </Button>
+        </Left>
+        <Body>
+          <Title>{item.name}</Title>
+        </Body>
+        <Right />
+      </Header>
+
+      <Content contentContainerStyle={{ justifyContent: 'center', flex: 1 }}>
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <H1 style={{ marginBottom: 15 }}>{item.name}</H1>
+
+          <Button bordered info onPress={() => Alert.alert('download')}>
+            <Text>Download</Text>
+          </Button>
+        </View>
+      </Content>
+    </Container>
   }
 }
 
