da8686e715a3cd6947ef45e43d8b7d991f8c134a
radinreth <radin.reth@gmail.com>
2019-11-01 16:05:22 +0700

    Enhance DownloadComponent

--- src/components/DownloadComponent.js
+++ src/components/DownloadComponent.js
@@ -4,22 +4,7 @@ import { View } from 'react-native'
 import { fileInfo } from '../config/utils'
 import { service } from '../services'
 
-import FileViewButton from './buttons/FileViewButton'
-import DownloadButton from './buttons/DownloadButton'
-import ProgressButton from './buttons/ProgressButton'
-
-const DownloadControl = ({status, progressedBytes, localUrl, handleDownload, isDisabled}) => {
-  switch(status) {
-    case '__BEGIN__':
-    case '__PROGRESS__':
-      return <ProgressButton progress={progressedBytes}/>
-    case '__DONE__':
-      return <FileViewButton localUrl={localUrl}/>
-    default:
-      return <DownloadButton  handleDownload={handleDownload} 
-                              isDisabled={isDisabled} />
-  }
-}
+import DownloadControl from './DownloadController'
 
 class DownloadComponent extends React.Component {
   constructor(props) {
@@ -33,6 +18,8 @@ class DownloadComponent extends React.Component {
       expectedBytes: 0.0,
       localUrl: ''
     }
+
+    this.handleDownload = this.handleDownload.bind(this)
   }
 
   async handleDownload () {
@@ -48,57 +35,42 @@ class DownloadComponent extends React.Component {
       service.downloadManager
         .download(remoteUrl, localUrl, fileDigest)
         .begin((expectedBytes) => {
-          let status = '__BEGIN__'
-          let isDisabled = true
-          this.setState({ isDisabled, expectedBytes, status})
+          this.setState({ isDisabled: true, expectedBytes, status: '__BEGIN__'})
         })
         .progress((progressedBytes) => {
-          let status = '__PROGRESS__'
-          this.setState({ status, progressedBytes })
+          this.setState({ status: '__PROGRESS__', progressedBytes })
         })
         .done(() => {
-          // this.saveToLocalDB(item.name, this.expectedBytes, filename)
-          // service.toastManager.show(`Downloaded completed!`)
-          let isDisabled = false
-          let status = '__DONE__'
-          this.setState({ isDisabled, localUrl, status })
+          this.saveToLocalDB()
+          this.setState({ isDisabled: false, localUrl, status: '__DONE__' })
         })
         .error( (error) => {
-          let status = '__ERROR__'
-          this.setState({ status, error }) 
+          this.setState({ status: '__ERROR__', error }) 
           console.log('download error:', error)
         })
     }
   }
 
-
-  async saveToLocalDB(fileName) {
-    let { expectedBytes } = this.state
-    let { database } = this.props
-
-    // let collection = new Collection(database, 'downloads')
-    // collection.save({ name: fileName, size: expectedBytes })
-    
+  async saveToLocalDB() {
+    let { database, item } = this.props
     let collection = await database.collections.get('downloads')
 
-    // let downloadDir = `${RNFS.ExternalStorageDirectoryPath}/Download`
-    // let localURL = `${downloadDir}/${filename}`
-    let { localUrl } = fileInfo(file)
+    let { expectedBytes } = this.state
+    let { fileName } = fileInfo(item)
 
     await database.action(async () => {
       await collection.create(dl => {
-        dl.name = name
+        dl.name = fileName
         dl.size = expectedBytes
       })
+      console.log('save to local')
     })
   }
 
   render() {
-    return <View>
-            <DownloadControl 
+    return <DownloadControl 
               {...this.state} 
-              handleDownload={() => this.handleDownload()} />
-          </View>
+              handleDownload={this.handleDownload} />
   }
 }
 
---
+++ src/components/DownloadController.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import FileViewButton from './buttons/FileViewButton'
+import DownloadButton from './buttons/DownloadButton'
+import ProgressButton from './buttons/ProgressButton'
+
+const DownloadControl = (props) => {
+  switch(props.status) {
+    case '__BEGIN__':
+    case '__PROGRESS__':
+      return <ProgressButton progress={props.progressedBytes}/>
+    case '__DONE__':
+      return <FileViewButton localUrl={props.localUrl}/>
+    default:
+      return <DownloadButton  handleDownload={props.handleDownload} 
+                              isDisabled={props.isDisabled} />
+  }
+}
+
+export default DownloadControl
