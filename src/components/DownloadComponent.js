5036e0dfbf9323851caac15153346b37e1a0de41
radinreth <radin.reth@gmail.com>
2019-11-08 10:55:20 +0700

    fix eslint DownloadComponent

--- src/components/DownloadComponent.js
+++ src/components/DownloadComponent.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { fileInfo } from '../config/utils'
-import { service } from '../services'
-import { connect } from 'react-redux'
-import DownloadControl from './DownloadController'
+import React from 'react';
+import {fileInfo} from '../config/utils';
+import {service} from '../services';
+import {connect} from 'react-redux';
+import DownloadControl from './DownloadController';
 
 class DownloadComponent extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       isTransparent: props.isTransparent,
@@ -15,15 +15,16 @@ class DownloadComponent extends React.Component {
       progressedBytes: 0.0,
       error: null,
       expectedBytes: 0.0,
-      localUrl: ''
-    }
+      localUrl: '',
+    };
 
-    this.handleDownload = this.handleDownload.bind(this)
+    this.handleDownload = this.handleDownload.bind(this);
   }
 
-  async handleDownload () {
-    let granted = await service.permissionManager.requestStorage()
+  async handleDownload() {
+    let granted = await service.permissionManager.requestStorage();
 
+<<<<<<< HEAD
     if( granted ) {
       let { item } = this.props
       let { remoteUrl, localUrl, fileName, mime } = fileInfo(item)
@@ -31,34 +32,60 @@ class DownloadComponent extends React.Component {
       service.firebaseManager.logEvent('EVENT_DOWNLOAD', { fileName })
 
       service.downloadManager.inTrayDownload(remoteUrl, fileName, mime)
+=======
+    if (granted) {
+      let {item} = this.props;
+      let {remoteUrl, localUrl, fileDigest, fileName} = fileInfo(item);
+
+      this.setState({isDisabled: true, localUrl});
+      service.firebaseManager.logEvent('EVENT_DOWNLOAD', {fileName});
+
+      service.downloadManager
+        .download(remoteUrl, localUrl, fileDigest)
+        .begin(expectedBytes => {
+          this.setState({isDisabled: true, expectedBytes, status: '__BEGIN__'});
+        })
+        .progress(progressedBytes => {
+          this.setState({status: '__PROGRESS__', progressedBytes});
+        })
+        .done(() => {
+          this.saveToLocalDB();
+          this.setState({isDisabled: false, localUrl, status: '__DONE__'});
+          service.toastManager.show('Download completed!');
+        })
+        .error(error => {
+          this.setState({status: '__ERROR__', error});
+          service.toastManager.show(`${error}, please try again later!`);
+        });
+>>>>>>> fix eslint DownloadComponent
     }
   }
 
   async saveToLocalDB() {
-    let { database, item } = this.props
-    let collection = await database.collections.get('downloads')
+    let {database, item} = this.props;
+    let collection = await database.collections.get('downloads');
 
-    let { expectedBytes } = this.state
-    let { fileName } = fileInfo(item)
+    let {expectedBytes} = this.state;
+    let {fileName} = fileInfo(item);
 
     await database.action(async () => {
       await collection.create(dl => {
-        dl.name = fileName
-        dl.size = expectedBytes
-      })
-    })
+        dl.name = fileName;
+        dl.size = expectedBytes;
+      });
+    });
   }
 
   render() {
-    return <DownloadControl 
-              {...this.state} 
-              handleDownload={this.handleDownload} />
+    return (
+      <DownloadControl {...this.state} handleDownload={this.handleDownload} />
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  let { database } = state
-  return { database }
-}
+const mapStateToProps = state => {
+  let {database} = state;
+  return {database};
+};
 
-export default connect(mapStateToProps)(DownloadComponent)
+export default connect(mapStateToProps)(DownloadComponent);
