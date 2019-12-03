ef26b288a3a7aeeae55ea07af113db829fb6de91
radinreth <radin.reth@gmail.com>
2019-12-03 13:43:07 +0700

    remove test AboutUs screen

--- __tests__/AboutUsScreen-Test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import AboutUsScreen from '../src/screens/AboutUsScreen'
-
-import renderer from 'react-test-renderer'
-
-it('render correctly', () => {
-  const tree = renderer.create(<AboutUsScreen />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
