40c5fb7a2149e0767c6afbce99d1328f37574960
radinreth <radin.reth@gmail.com>
2019-12-13 12:35:12 +0700

    fix warning eslint for Root

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,42 +1,47 @@
-import React from 'react'
-import { service } from '../services'
-import firebase from 'react-native-firebase'
-import AsyncStorage from '@react-native-community/async-storage'
-import {withNavigation} from 'react-navigation'
+import React from 'react';
+import {service} from '../services';
+import firebase from 'react-native-firebase';
+import AsyncStorage from '@react-native-community/async-storage';
+import {withNavigation} from 'react-navigation';
 
 class Root extends React.Component {
-
   async componentDidMount() {
-    this.checkPermission()
-    this.createNotificationListeners()
-    await service.permissionManager.requestStorage()
-    await firebase.crashlytics().enableCrashlyticsCollection()
-    await firebase.analytics().setAnalyticsCollectionEnabled(true)
+    this.checkPermission();
+    this.createNotificationListeners();
+    await service.permissionManager.requestStorage();
+    await firebase.crashlytics().enableCrashlyticsCollection();
+    await firebase.analytics().setAnalyticsCollectionEnabled(true);
   }
 
   checkPermission() {
-    firebase.messaging().hasPermission()
-    .then(enabled => {
-      if (enabled) {
-        this.getToken();
-      } else {
-        this.requestPermission()
-      } 
-    });
+    firebase
+      .messaging()
+      .hasPermission()
+      .then(enabled => {
+        if (enabled) {
+          this.getToken();
+        } else {
+          this.requestPermission();
+        }
+      });
   }
 
   async getToken() {
-    let fcmToken = await AsyncStorage.getItem("fcmToken");
+    let fcmToken = await AsyncStorage.getItem('fcmToken');
     if (!fcmToken) {
       fcmToken = await firebase.messaging().getToken();
       if (fcmToken) {
-        await AsyncStorage.setItem("fcmToken", fcmToken);
-        service.apiManager.saveToken(fcmToken)
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+        service.apiManager.saveToken(fcmToken);
 
-        const channel = new firebase.notifications.Android.Channel('eoc-channel', 'EOC Channel', firebase.notifications.Android.Importance.Max)
-                              .setDescription('ilabsoutheastasia.org/eoc')
-                              .setSound('notif')
-        
+        const channel = new firebase.notifications.Android.Channel(
+          'eoc-channel',
+          'EOC Channel',
+          firebase.notifications.Android.Importance.Max,
+        )
+          .setDescription('ilabsoutheastasia.org/eoc')
+          .setSound('notif');
+
         firebase.notifications().android.createChannel(channel);
       }
     }
@@ -47,7 +52,7 @@ class Root extends React.Component {
       await firebase.messaging().requestPermission();
       this.getToken();
     } catch (e) {
-      service.toastManager.show(e)
+      service.toastManager.show(e);
     }
   }
 
@@ -58,52 +63,59 @@ class Root extends React.Component {
   }
 
   handleDetailNavigate(listener) {
-    if( listener ) {
-      const { data } = listener.notification
-      let itemId = data.itemId
-      this.props.navigation.navigate("SopDetail", { itemId })
+    if (listener) {
+      const {data} = listener.notification;
+      let itemId = data.itemId;
+      this.props.navigation.navigate('SopDetail', {itemId});
     }
   }
 
   createNotificationListeners() {
     /*
-    * app is in foreground
-    * */
-    this.removeNotificationListener = firebase.notifications().onNotification((notification) => {
-      const { data } = notification;
-      let params = { payload: data, navigation: this.props.navigation }
-      service.toastManager.show("New notification!", params)
-    });
-  
-    /*
-    * app is in background
-    * */
-    this.removeNotificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
-      this.handleDetailNavigate(notificationOpen)
-    });
-
+     * app is in foreground
+     * */
+    this.removeNotificationListener = firebase
+      .notifications()
+      .onNotification(notification => {
+        const {data} = notification;
+        let params = {payload: data, navigation: this.props.navigation};
+        service.toastManager.show('New notification!', params);
+      });
 
     /*
-    * subscribe to a topic
-    * */
-    this.removeMessageListener = firebase.messaging().onMessage((notification) => {
-      const { data } = notification;
-      let params = { payload: data, navigation: this.props.navigation }
-      service.toastManager.show("New notification!", params)
-    });
-  
+     * app is in background
+     * */
+    this.removeNotificationOpenedListener = firebase
+      .notifications()
+      .onNotificationOpened(notificationOpen => {
+        this.handleDetailNavigate(notificationOpen);
+      });
+
     /*
-    * app is closed
-    * */
-    firebase.notifications().getInitialNotification()
+     * subscribe to a topic
+     * */
+    this.removeMessageListener = firebase
+      .messaging()
+      .onMessage(notification => {
+        const {data} = notification;
+        let params = {payload: data, navigation: this.props.navigation};
+        service.toastManager.show('New notification!', params);
+      });
+
+    /*
+     * app is closed
+     * */
+    firebase
+      .notifications()
+      .getInitialNotification()
       .then(notificationInitial => {
-        this.handleDetailNavigate(notificationInitial)
-      })
+        this.handleDetailNavigate(notificationInitial);
+      });
   }
 
   render() {
-    return this.props.children
+    return this.props.children;
   }
 }
 
-export default withNavigation(Root)
+export default withNavigation(Root);
